9e83bff26343ed6c68209fc21a02e95f83e76758
Rodrigo <rodrigopf2@hotmail.com>
2023-03-22 15:50:26 -0400

    testando git+VS2019

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿namespace xadrez_console.tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, int qteMovimentos, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.qteMovimentos = 0;
+            this.tab = tab;
+        }
+    }
+}
---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linha { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas { get; set; }
+    }
+}
