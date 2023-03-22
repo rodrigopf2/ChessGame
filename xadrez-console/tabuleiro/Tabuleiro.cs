c43d498f3b60fe8cd71c361bb2d79529efea5c39
Rodrigo <rodrigopf2@hotmail.com>
2023-03-22 17:59:29 -0400

    Comecando a implementar o metodo que imprime o tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -8,15 +8,20 @@ namespace xadrez_console.tabuleiro
 {
     class Tabuleiro
     {
-        public int linha { get; set; }
+        public int linhas { get; set; }
         public int colunas { get; set; }
         private Peca[,] pecas { get; set; }
 
-        public Tabuleiro(int linha, int colunas)
+        public Tabuleiro(int linhas, int colunas)
         {
-            this.linha = linha;
+            this.linhas = linhas;
             this.colunas = colunas;
-            pecas = new Peca[linha, colunas];
+            pecas = new Peca[linhas, colunas];
+        }
+
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }
