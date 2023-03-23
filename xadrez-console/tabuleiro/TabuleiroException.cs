8e14383ab69f115724a0d8404d061b5bca27e66c
Rodrigo <rodrigopf2@hotmail.com>
2023-03-23 09:23:11 -0400

    tratando erros com execao personalizada

---
+++ xadrez-console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace xadrez_console.tabuleiro
+{
+    class TabuleiroException :Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
